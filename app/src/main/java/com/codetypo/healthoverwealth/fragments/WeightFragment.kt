3382bb29695d20836bca606d628b4878fc03a87c
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-05-18 08:56:46 +0200

    Initial BMI calculation based on a constant mass and an interpretation of the calculation result were created.

--- app/src/main/java/com/codetypo/healthoverwealth/fragments/WeightFragment.kt
+++ app/src/main/java/com/codetypo/healthoverwealth/fragments/WeightFragment.kt
@@ -8,13 +8,20 @@ import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.codetypo.healthoverwealth.R
+import com.codetypo.healthoverwealth.communicator.Communicator
 import kotlinx.android.synthetic.main.fragment_weight.*
-
 
 class WeightFragment : Fragment() {
 
     var weightInterface: WeightFragmentInterface? = null
+
+    companion object {
+        lateinit var mctx: Context
+    }
+
+    private var model: Communicator? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,14 +48,28 @@ class WeightFragment : Fragment() {
 
         simpleSeekBar?.setOnSeekBarChangeListener(object :
             OnSeekBarChangeListener {
+
+            var progress1 = 0f
+
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 tvWeight!!.text = (progress / 10.0).toString()
+
+                progress1 = ((progress / 10.0).toFloat())
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
             }
 
             override fun onStopTrackingTouch(seek: SeekBar) {
+                model = ViewModelProviders.of(activity!!).get(Communicator::class.java)
+
+                model!!.setWeight("" + progress1)
+
+                val myfragment = BmiFragment()
+                val fragmentTransaction = fragmentManager!!.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentBmi, myfragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
             }
         })
     }
