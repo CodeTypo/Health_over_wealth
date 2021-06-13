a34c0bc1b14c9a2cd847f6ac1d3d3ebbd129699c
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-06-13 14:18:09 +0200

    All the saved values to the database are assigned to the appropriate user.

--- app/src/main/java/com/codetypo/healthoverwealth/fragments/WeightFragment.kt
+++ app/src/main/java/com/codetypo/healthoverwealth/fragments/WeightFragment.kt
@@ -2,6 +2,7 @@ package com.codetypo.healthoverwealth.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,8 @@ import androidx.lifecycle.ViewModelProviders
 import com.codetypo.healthoverwealth.R
 import com.codetypo.healthoverwealth.communicator.Communicator
 import com.codetypo.healthoverwealth.models.WeightModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -49,7 +52,9 @@ class WeightFragment : Fragment() {
 
         val database = FirebaseDatabase.getInstance()
 
-        val weightModel = database.reference.child("WeightModel")
+        val uuid = FirebaseAuth.getInstance().currentUser?.uid
+
+        val weightModel = database.reference.child(uuid.toString()).child("WeightModel")
 
         weightModel.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
