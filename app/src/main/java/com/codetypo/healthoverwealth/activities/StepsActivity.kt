449f5db0ba7dddb1d77fd93e1efbff49322e99ea
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-06-13 16:52:15 +0200

    Fixed steps counter.

--- app/src/main/java/com/codetypo/healthoverwealth/activities/StepsActivity.kt
+++ app/src/main/java/com/codetypo/healthoverwealth/activities/StepsActivity.kt
@@ -1,6 +1,5 @@
 package com.codetypo.healthoverwealth.activities
 
-import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -21,92 +20,94 @@ import kotlinx.android.synthetic.main.activity_steps.*
 
 
 class StepsActivity : AppCompatActivity(), SensorEventListener {
+
+    var running = false
     var sensorMgr: SensorManager? = null
-    var stepCounter : Sensor? = null;
     var stepsTV: TextView? = null
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_steps)
-            setBarChart()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_steps)
+        setBarChart()
 
-            stepsTV = stepCounterTV;
-            sensorMgr = this.getSystemService(SENSOR_SERVICE) as SensorManager
-            stepCounter = sensorMgr!!.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        }
+        stepsTV = stepCounterTV
+        sensorMgr = getSystemService(SENSOR_SERVICE) as SensorManager
+    }
 
-        private fun setBarChart() {
-            val entries = ArrayList<BarEntry>()
-            entries.add(BarEntry(1f, 10000f))
-            entries.add(BarEntry(2f, 12000f))
-            entries.add(BarEntry(3f, 8000f))
-            entries.add(BarEntry(4f, 13125f))
-            entries.add(BarEntry(5f, 4500f))
-            entries.add(BarEntry(6f, 5750f))
-            entries.add(BarEntry(7f, 13000f))
+    private fun setBarChart() {
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(1f, 10000f))
+        entries.add(BarEntry(2f, 12000f))
+        entries.add(BarEntry(3f, 8000f))
+        entries.add(BarEntry(4f, 13125f))
+        entries.add(BarEntry(5f, 4500f))
+        entries.add(BarEntry(6f, 5750f))
+        entries.add(BarEntry(7f, 13000f))
 
-            val barDataSet = BarDataSet(entries, "Cells")
+        val barDataSet = BarDataSet(entries, "Cells")
 
-            val data = BarData(barDataSet)
-            barChart.data = data // set the data and list of lables into chart
-            //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
-            barDataSet.color = resources.getColor(R.color.teal_200)
+        val data = BarData(barDataSet)
+        barChart.data = data // set the data and list of lables into chart
+        //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
+        barDataSet.color = resources.getColor(R.color.teal_200)
 
-            val left: YAxis = barChart.axisLeft
-            left.setDrawAxisLine(false) // no axis line
-            left.setDrawGridLines(false) // no grid lines
+        val left: YAxis = barChart.axisLeft
+        left.setDrawAxisLine(false) // no axis line
+        left.setDrawGridLines(false) // no grid lines
 
-            val right: YAxis = barChart.axisRight
-            right.setDrawAxisLine(false) // no axis line
-            right.setDrawGridLines(false) // no grid lines
+        val right: YAxis = barChart.axisRight
+        right.setDrawAxisLine(false) // no axis line
+        right.setDrawGridLines(false) // no grid lines
 
-            val xAxis: XAxis = barChart.xAxis
-            xAxis.setDrawLabels(false);
-            xAxis.setDrawGridLines(false);
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.setDrawLabels(false);
+        xAxis.setDrawGridLines(false);
 
-            barChart.animateY(3000)
-            val l1 = LegendEntry()
-            l1.label = "mon"
-            val l2 = LegendEntry()
-            l2.label = "tue"
-            val l3 = LegendEntry()
-            l3.label = "wed"
-            val l4 = LegendEntry()
-            l4.label = "thu"
-            val l5 = LegendEntry()
-            l5.label = "fri"
-            val l6 = LegendEntry()
-            l6.label = "sat"
-            val l7 = LegendEntry()
-            l7.label = "sun"
-            val l: Legend = barChart.getLegend()
-            l.setCustom(listOf(l1,l2,l3,l4,l5,l6,l7));
-            l.xEntrySpace = 10f; // space between the legend entries on the x-axis
-        }
+        barChart.animateY(3000)
+        val l1 = LegendEntry()
+        l1.label = "mon"
+        val l2 = LegendEntry()
+        l2.label = "tue"
+        val l3 = LegendEntry()
+        l3.label = "wed"
+        val l4 = LegendEntry()
+        l4.label = "thu"
+        val l5 = LegendEntry()
+        l5.label = "fri"
+        val l6 = LegendEntry()
+        l6.label = "sat"
+        val l7 = LegendEntry()
+        l7.label = "sun"
+        val l: Legend = barChart.getLegend()
+        l.setCustom(listOf(l1, l2, l3, l4, l5, l6, l7));
+        l.xEntrySpace = 10f; // space between the legend entries on the x-axis
+    }
 
     override fun onResume() {
         super.onResume()
-        sensorMgr!!.registerListener(this, stepCounter, 2000000)
+        running = true
+        var stepsSensor = sensorMgr?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+
+        } else {
+            sensorMgr?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
     }
 
     override fun onPause() {
         super.onPause()
+        running = false
         sensorMgr!!.unregisterListener(this)
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            Log.d("SENSOR", event.values[0].toString())
+    override fun onSensorChanged(event: SensorEvent) {
+        if (running) {
             stepsTV?.text = event.values[0].toString()
-//            if (event.values[0] > (weeklyGoalTV.text.toString()).toFloat()){
-//                Log.d("STEPS_ACTIV","goal accomplished")
-//                goalProgressTV.text = "Goal Achieved!"
-//                goalProgressTV.setTextColor(Color.GREEN)
-//            }
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        Log.d("ACC","changed")
+        Log.d("ACC", "changed")
     }
 }
