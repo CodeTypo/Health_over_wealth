ab3e4352bf5507aaf938451150ff6526f7d27636
Dawid Kupczyk <250065@student.pwr.edu.pl>
2021-06-15 00:25:50 +0200

    Heart Rate monitor works (mocked, so far)

--- app/src/main/java/com/codetypo/healthoverwealth/activities/HeartActivity.kt
+++ app/src/main/java/com/codetypo/healthoverwealth/activities/HeartActivity.kt
@@ -1,21 +1,31 @@
 package com.codetypo.healthoverwealth.activities
 
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.codetypo.healthoverwealth.R
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_heart.*
+import kotlinx.android.synthetic.main.activity_steps.*
 import kotlin.math.roundToInt
+
 
 class HeartActivity : AppCompatActivity(), SensorEventListener {
     var sensorMgr: SensorManager? = null
     var heartRate : Sensor? = null;
     var heartTV: TextView? = null
+    var thread: Thread? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +36,97 @@ class HeartActivity : AppCompatActivity(), SensorEventListener {
         heartRate = sensorMgr!!.getDefaultSensor(Sensor.TYPE_HEART_RATE);
         Log.d("HR_ACTIV","CREATED")
 
+        var data = LineData()
+        data.setValueTextColor(Color.RED)
+        heart_chart.data = data;
+        var legend = heart_chart.legend
+        legend.form= Legend.LegendForm.LINE
+        legend.textColor = Color.BLACK
+        legend.isEnabled = false
+        val xl = heart_chart.xAxis
+        xl.textColor = Color.BLACK
+        xl.setDrawGridLines(false)
+        xl.setAvoidFirstLastClipping(true)
+        xl.isEnabled = false
 
+        val leftAxis =heart_chart.axisLeft
+        leftAxis.textColor = Color.BLACK
+        leftAxis.axisMinimum = 30f
+        leftAxis.axisMaximum = 150f
+        leftAxis.setDrawGridLines(false)
+        leftAxis.isEnabled = false
+
+        val d = Description()
+        d.text=""
+        heart_chart.description= d
+
+        val rightAxis = heart_chart.axisRight
+        rightAxis.isEnabled = false
+
+        feedMultiple()
+
+    }
+
+    private fun createSet(): LineDataSet? {
+        val set = LineDataSet(null, "Heart rate")
+        set.axisDependency = YAxis.AxisDependency.LEFT
+        set.color = Color.RED
+        set.setCircleColor(Color.RED)
+        set.lineWidth = 3f
+        set.circleRadius = 0f
+        set.fillAlpha = 65
+        set.fillColor = Color.RED
+        set.highLightColor = Color.rgb(244, 117, 117)
+        set.valueTextColor = Color.WHITE
+        set.valueTextSize = 0f
+        set.setDrawValues(false)
+        return set
+    }
+
+
+    private fun addEntry() {
+        val data: LineData = heart_chart.getData()
+        if (data != null) {
+            var set = data.getDataSetByIndex(0)
+            if (set == null) {
+                set = createSet()
+                data.addDataSet(set)
+            }
+
+            val entryvalue = ((Math.random() * 40) + 30f)
+
+            data.addEntry((Entry(set.entryCount.toFloat(),
+                entryvalue.toFloat())),0)
+            heartMonitorTV.text = entryvalue.toInt().toString()
+            data.notifyDataChanged()
+
+            // let the graph know it's data has changed
+            heart_chart.notifyDataSetChanged()
+
+            // limit the number of visible entries
+            heart_chart.setVisibleXRangeMaximum(40F)
+
+            // move to the latest entry
+            heart_chart.moveViewToX(data.entryCount.toFloat())
+        }
+    }
+
+    private fun feedMultiple() {
+        thread?.interrupt()
+        val runnable = Runnable { addEntry() }
+        thread = Thread {
+            for (i in 0..999) {
+
+                // Don't generate garbage runnables inside the loop.
+                runOnUiThread(runnable)
+                try {
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        thread!!.start()
     }
 
     override fun onResume() {
